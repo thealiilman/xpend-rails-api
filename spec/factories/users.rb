71a27c186d1f2fab5f1d9f0b861a332cfbe11384
Ali Ilman <aliilman532@gmail.com>
2019-07-21 14:52:23 +0800

    Fixed user factory creating username not within length validation. (#18)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    username { Faker::Internet.username(nil, ['_']) }
+    username { Faker::Internet.username(2..16, ['_']) }
     sequence(:email) { |n| "user#{n}@email.com" }
     password { 'password' }
   end
