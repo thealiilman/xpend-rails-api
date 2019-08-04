6360c38c94feabcc81d3fc2e6d3f5aa0482d814a
thealiilman <ali@ali-ilman.com>
2019-08-04 15:40:38 +0800

    Added user can create expenses feature.

---
+++ spec/requests/expenses/create_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+describe Api::ExpensesController, type: :request do
+  path '/expenses' do
+    post 'Create an expense' do
+      tags 'Expenses'
+      consumes 'application/json'
+      produces 'application/json'
+
+      parameter name: 'Authorization',
+                in: :header, type: :string, required: true
+
+      parameter name: :expense, in: :body, schema: {
+        type: :object,
+        properties: {
+          title: { type: :string },
+          description: { type: :string },
+          amount_cents: { type: :integer },
+          amount_currency: { type: :string },
+          user_id: { type: :integer },
+          expense_category_id: { type: :integer }
+        },
+        required: %i[title amount_cents user_id expense_category_id]
+      }
+
+      let(:Authorization) do
+        "Bearer #{KnockToken.generate(user.id).token}"
+      end
+
+      response '201', 'returns created expense' do
+        let(:user) { create(:user) }
+        let(:expense_category) { create(:expense_category) }
+        let(:expense) do
+          build(:expense, user: user, expense_category: expense_category)
+        end
+
+        run_test! do
+          expect(json['data']['attributes']['title']).to eq(expense.title)
+          expect(json['data']['attributes']['description'])
+            .to eq(expense.description)
+          expect(json['data']['attributes']['amount_cents'])
+            .to eq(expense.amount_cents)
+          expect(json['data']['attributes']['amount_currency'])
+            .to eq(expense.amount_currency)
+          expect(json['data']['attributes']['user_id'])
+            .to eq(expense.user_id)
+          expect(json['included'].first['attributes']['title'])
+            .to eq(expense_category.title)
+        end
+      end
+
+      response '422', 'returns unprocessable entity error' do
+        let(:user) { create(:user) }
+        let(:expense) do
+          {
+            title: build_stubbed(:expense).title
+          }
+        end
+
+        run_test!
+      end
+    end
+  end
+end
