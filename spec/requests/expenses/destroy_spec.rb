125af62e22155ab5788524361ddd844cdec164a7
thealiilman <ali@ali-ilman.com>
2019-08-04 18:01:38 +0800

    Added user can destroy an expense feature.

---
+++ spec/requests/expenses/destroy_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe Api::ExpensesController, type: :request do
+  path '/expenses/{expense_id}' do
+    delete 'Destroy an expense' do
+      tags 'Expenses'
+
+      parameter name: 'Authorization',
+                in: :header, type: :string, required: true
+      parameter name: :expense_id, in: :path, type: :string, required: true
+
+      let(:Authorization) { "Bearer #{KnockToken.generate(user.id).token}" }
+      let(:user) { create(:user) }
+      let(:expense) { create(:expense, user: user) }
+      let(:expense_id) { expense.id }
+
+      response '200', 'returns ok' do
+        run_test!
+      end
+    end
+  end
+end
