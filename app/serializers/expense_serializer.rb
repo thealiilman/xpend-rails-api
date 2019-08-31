e4f7fe1cc7c9cb855ad42d9f6f6d0a7f98cfcbbc
thealiilman <ali@ali-ilman.com>
2019-08-31 16:53:07 +0800

    Added optional currency filter to pass to fetch all expenses API.

--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -1,11 +1,18 @@
 class ExpenseSerializer < ApplicationSerializer
   attributes :title,
              :description,
-             :user_id,
-             :amount_currency
+             :user_id
 
-  attribute :amount do |object|
-    object.amount.to_f
+  attribute :amount do |object, params|
+    if params[:currency]
+      object.amount.exchange_to(params[:currency]).to_f
+    else
+      object.amount.to_f
+    end
+  end
+
+  attribute :amount_currency do |object, params|
+    params[:currency] || object.amount_currency
   end
 
   attribute :expense_category do |object|
